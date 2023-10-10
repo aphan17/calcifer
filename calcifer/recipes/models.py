7bc3496a9d2f4f80553ef4e1a4e81d45588e4690
ashley phan <ashleyphan17@gmail.com>
2023-10-10 15:45:07 -0700

    added react framework

--- calcifer/recipes/models.py
+++ calcifer/recipes/models.py
@@ -4,3 +4,27 @@ from django.db import models
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+
+
+class RecipeStep(models.Model):
+    number = models.PositiveSmallIntegerField()
+    instruction = models.TextField()
+    recipe = models.ForeignKey(
+        "Recipe",
+        related_name='steps',
+        on_delete=models.CASCADE,
+    )
+    class Meta:
+        ordering = ["number"]
+
+
+class RecipeIngredient(models.Model):
+    amount = models.CharField(max_length=150)
+    item = models.CharField(max_length=150)
+    recipe = models.ForeignKey(
+        "Recipe",
+        related_name='ingredients',
+        on_delete=models.CASCADE,
+    )
+    class Meta:
+        ordering = ["item"]
