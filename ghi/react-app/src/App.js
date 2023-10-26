6425446a9b6e5c6946dc9c729d7303b6a905a3b8
ashley phan <ashleyphan17@gmail.com>
2023-10-26 15:42:18 -0700

    installed react router dom

--- ghi/react-app/src/App.js
+++ ghi/react-app/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Nav/>
         <Routes>
           <Route path="/" element={<MainPage/>}/>
         </Routes>
