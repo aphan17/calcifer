98b3d15f286056c775f0a4529fdaa78bc4d27c99
ashley phan <ashleyphan17@gmail.com>
2023-10-31 15:15:20 -0700

    fetch recipes function

--- ghi/react-app/src/recipes/RecipeList.js
+++ ghi/react-app/src/recipes/RecipeList.js
@@ -1,4 +1,23 @@
+import {useState, useEffect} from 'react'
+
+
 const RecipeList = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    const getRecipes = async () => {
+        const url = "http://localhost:8000/recipes/"
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = response.json();
+            const recipes = data.recipes;
+            setRecipes(recipes);
+        }
+    }
+    useEffect(() => {
+        getRecipes();
+    }, [])
+
+
     return (
         <div>
             recipe listed here
