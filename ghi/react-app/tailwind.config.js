2ce7de286f61de20c0452f7ebec9e3b53407b8fe
ashley phan <ashleyphan17@gmail.com>
2023-11-01 14:43:22 -0700

    added tailwind

---
+++ ghi/react-app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".src/App.js", ".src/index.js", ".src/MainPage.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
