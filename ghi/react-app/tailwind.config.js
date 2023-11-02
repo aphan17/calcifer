1113c443c46cf92330cfecf5c0869eb67bef91b7
ashley phan <ashleyphan17@gmail.com>
2023-11-02 11:50:40 -0700

    got tailwind to work

--- ghi/react-app/tailwind.config.js
+++ ghi/react-app/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".src/App.js", ".src/index.js", ".src/MainPage.js"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
