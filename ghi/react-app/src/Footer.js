4ba63c4b28a52c43b4d708b0cbd08787a8a6f7b1
ashley phan <ashleyphan17@gmail.com>
2023-12-04 13:29:09 -0800

    footer updated

--- ghi/react-app/src/Footer.js
+++ ghi/react-app/src/Footer.js
@@ -2,9 +2,9 @@ function Footer() {
     return (
     <footer class="fixed bottom-0 rounded-lg m-4">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="#" class="hover:underline"></a>. All Rights Reserved.
+        <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="#" class="hover:underline"></a> Calcifer by APhan.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+        <ul class="flex flex-wrap items-center ml-3 mt-3 text-sm font-medium text-gray-500 sm:mt-0">
             <li>
                 <a href="#" class="hover:underline me-4 md:me-6">About</a>
             </li>
