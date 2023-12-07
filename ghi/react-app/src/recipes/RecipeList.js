5caa93cf3eda0eea674870dbbad87395e3b7b89c
ashley phan <ashleyphan17@gmail.com>
2023-12-07 14:31:07 -0800

    flowbite plugin

--- ghi/react-app/src/recipes/RecipeList.js
+++ ghi/react-app/src/recipes/RecipeList.js
@@ -3,26 +3,23 @@ import {useState, useEffect} from 'react'
 
 export function RecipeCard(props) {
     return (
-        <div className= "flex justify-center">
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+        <div className="flex-1 max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+            <a href="#">
+                <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+            </a>
+            <div className="p-5">
                 <a href="#">
-                    <img className="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{props.recipe.name}</h5>
                 </a>
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{props.recipe.name}</h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.recipe.description}</p>
-                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none">
-                        Read more
-                        <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                        </svg>
-                    </a>
-                </div>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.recipe.description}</p>
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none">
+                    Read more
+                    <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                    </svg>
+                </a>
             </div>
         </div>
-
     );
 }
 
@@ -50,11 +47,11 @@ function RecipeList() {
                 Here is the recipe index, browse by category or course.
                 </p>
             </div>
-            <nav className="nav-recipes">
+            <nav className="nav-recipes flex py-2 px-4">
                 <div>
                     <ul>
                         <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">By Category<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"  fill="none" viewBox="0 0 10 6">
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">By Category<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"  fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                             </svg>
                             </button>
@@ -62,7 +59,7 @@ function RecipeList() {
                             <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
                                 <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownLargeButton">
                                 <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Protein</a>
+                                    <a href="#" class="block px-4 py-3 hover:bg-gray-100">Protein</a>
                                 </li>
                                 <li>
                                     <a href="#" class="block px-4 py-2 hover:bg-gray-100">Veggies</a>
@@ -101,7 +98,7 @@ function RecipeList() {
 
             </nav>
 
-            <div className="recipes">
+            <div className="recipes flex flex-wrap justify-center space-x-4 px-4 py-3">
             {recipes.map((recipe) => (
                 <RecipeCard recipe={recipe} key={recipe.id} />
             ))}
