46eaa6729b60a0cd18ed69f1ec5a32647043f27d
ashley phan <ashleyphan17@gmail.com>
2024-02-05 13:07:09 -0800

    changed category nav

--- ghi/react-app/src/Nav.js
+++ ghi/react-app/src/Nav.js
@@ -27,8 +27,8 @@ function Nav() {
                     <li>
                     <a href="#" className="block mt-7 mx-4 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
                     </li>
-                </ul>
-                </div>
+                    </ul>
+                    </div>
             </div>
         </nav>
         <div className="header">
--- ghi/react-app/src/recipes/RecipeList.js
+++ ghi/react-app/src/recipes/RecipeList.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-
+import "./recipe.css";
 
 export function RecipeCard(props) {
     return (
@@ -47,56 +47,19 @@ function RecipeList() {
                 Here is the recipe index, browse by category or course.
                 </p>
             </div>
-            <nav className="nav-recipes flex py-2 px-4">
-                <div>
-                    <ul>
-                        <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-3 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">By Category<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"  fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                            </svg>
-                            </button>
-                            {/* Dropdown Menu */}
-                            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
-                                <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownLargeButton">
-                                <li>
-                                    <a href="#" class="block px-4 py-3 hover:bg-gray-100">Protein</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Veggies</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Savory Carbs</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Sweets</a>
-                                </li>
-                                </ul>
-                            </div>
-                        </li>
-                        <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto">By Course<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true"  fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-                            </svg>
-                            </button>
-                            {/* Dropdown Menu */}
-                            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
-                                <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownLargeButton">
-                                <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Breakfast</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Lunch</a>
-                                </li>
-                                <li>
-                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100">Desserts</a>
-                                </li>
-                                </ul>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
 
-            </nav>
+            <div>
+                <nav className="recipeNav">
+                    <ul className="recipeCategories">
+                        <li className="recipeList"></li>
+                            <button>Category</button>
+                        <li className="recipeList">Collection</li>
+                            <button>Category</button>
+                        <li className="recipeList">Course</li>
+                    </ul>
+                </nav>
+
+            </div>
 
             <div className="recipes flex flex-wrap justify-center space-x-4 px-4 py-3">
             {recipes.map((recipe) => (
